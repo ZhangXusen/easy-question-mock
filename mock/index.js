ae900038fa461cd9737024105b915bef297780df
zxs <461694365@qq.com>
2023-09-02 22:50:24 +0800

    问题mock

---
+++ mock/index.js
@@ -0,0 +1,8 @@
+const question = require("./question");
+// const user = require("./user");
+// const stat = require("./stat");
+// const answer = require("./answer");
+
+const mockList = [...question];
+
+module.exports = mockList;
