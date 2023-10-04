6980de7e5729248a09059192f848bef66668372d
zxs <461694365@qq.com>
2023-10-04 16:24:47 +0800

    fix: forget require

--- mock/index.js
+++ mock/index.js
@@ -1,6 +1,15 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Author: 小国际
+ * @Date: 2023-09-02 21:17:52
+ * @LastEditors: 小国际
+ * @LastEditTime: 2023-10-04 16:20:41
+ */
 const question = require("./question");
+const stat = require("./stat");
 const user = require("./user");
 
-const mockList = [...question, ...user];
+const mockList = [...question, ...user, ...stat];
 
 module.exports = mockList;
